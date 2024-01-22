d264f887da045df28c30821c194503727572000a
AlessiaOsman <alessia.osman@gmail.com>
2024-01-22 17:46:55 +0100

    goToNext and goToPrev

---
+++ js/script.js
@@ -0,0 +1,34 @@
+console.log('vue ok', Vue)
+
+const {createApp} = Vue;
+
+const app = createApp({
+    data (){
+        return {
+            destinations,
+            currentIndex: 0,
+            autoPlay: null
+        }
+    },
+    computed: {
+        isFirstIndex(){
+            return this.currentIndex === 0
+        },
+
+        isLastIndex(){
+            return this.currentIndex === this.destinations.length -1
+        }
+    },
+    methods : {
+        goToPrev(){
+            if(!this.isFirstIndex) this.currentIndex--
+            else this.currentIndex = this.destinations.length -1
+        },
+        goToNext(){
+           if(!this.isLastIndex) this.currentIndex++
+           else this.currentIndex = 0
+        }
+    },
+})
+
+app.mount('#root')
